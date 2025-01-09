52a635fc37818de62c10fd411447c24479f28558
Swati-Kumari-Das <swatikumaridas1111@gmail.com>
2025-01-09 14:02:26 +0530

    new files

---
+++ app.js
@@ -0,0 +1,46 @@
+let btn=document.querySelector("button");
+let ul=document.querySelector("ul");
+let inp=document.querySelector("input");
+
+btn.addEventListener("click", function(){
+    let item=document.createElement("li");
+    item.innerText= inp.value;
+
+
+    let delBtn=document.createElement("button");
+    delBtn.innerText="delete";
+    delBtn.classList.add("delete");
+
+    item.appendChild(delBtn);
+    ul.appendChild(item);
+    console.log(inp.value);
+    inp.value="";
+
+});
+
+ul.addEventListener("click", function(event){
+    // console.dir(event.target.nodeName); --> button
+    // console.log("button clicked!");
+    
+    if (event.target.nodeName == "BUTTON") {
+        let listItem=event.target.parentElement;
+        // console.log(listItem);
+        listItem.remove();
+        console.log("deleted");
+    } 
+    // else{
+    //     console.log("don't delete");
+    // }
+
+});
+//event deleggation bubbling 
+
+// let delBtns=document.querySelectorAll(".delete");
+// for (delBtn of delBtns) {
+//     delBtn.addEventListener("click", function(){
+//      let par=this.parentElement;
+//      console.log(par);
+//      par.remove();
+//     });
+    
+// }
